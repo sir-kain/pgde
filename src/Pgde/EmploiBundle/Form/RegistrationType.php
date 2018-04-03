e8eec3b7f1944c3e1ac0fff30619ee0296f50cc3
Ahmadou WAly Ndiaye <ahmadouwalyndiaye@gmail.com>
2018-04-03 15:21:09 +0000

    Mise en place du mailing ET creation de la classe utilisateur

---
+++ src/Pgde/EmploiBundle/Form/RegistrationType.php
@@ -0,0 +1,34 @@
+<?php
+//
+namespace Pgde\EmploiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('numberid');
+        $builder->add('firstname');
+        $builder->add('lastname');
+    }
+
+    public function getParent()
+
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+
+    {
+        return 'app_user_registration';
+    }
+
+    public function getName()
+
+    {
+        return $this->getBlockPrefix();
+    }
+}
