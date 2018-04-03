e5132457a9799453913efd5cf5f6d10c6453aa39
Ahmadou WAly Ndiaye <ahmadouwalyndiaye@gmail.com>
2018-04-03 14:11:43 +0000

    Installation et Configuration de FOSUserBundle

---
+++ src/Pgde/EmploiBundle/Entity/Utilisateur.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pgde\EmploiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Utilisateur
+ *
+ * @ORM\Table(name="utilisateur")
+ * @ORM\Entity(repositoryClass="Pgde\EmploiBundle\Repository\UtilisateurRepository")
+ */
+class Utilisateur extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
