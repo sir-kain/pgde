29c24bd0d58d68f6729897f677252f7c4268b309
Ahmadou WAly Ndiaye <ahmadouwalyndiaye@gmail.com>
2018-04-17 22:39:55 +0000

    update parameters_production.php

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -3,7 +3,7 @@ $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', 'johnny.heliohost.org');
 $container->setParameter('database_port', 3306);
 $container->setParameter('database_name', 'ahmadouw_pgde');
-$container->setParameter('database_user', 'ahmadouw_pgde');
+$container->setParameter('database_user', 'ahmadouw_userfp');
 $container->setParameter('database_password', '6703903awn');
 $container->setParameter('secret', 'db1b548114372a7701e95e2282052750165a6241');
 $container->setParameter('locale', 'fr');
