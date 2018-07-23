955e303eb642a90def161115954c832feadf6c3b
Sir Kane <ahmadouwalyndiaye@gmail.com>
2018-07-23 10:58:50 +0000

    [Admin] update de la liste des demandeurs

--- src/Pgde/EmploiBundle/Admin/UserdataAdmin.php
+++ src/Pgde/EmploiBundle/Admin/UserdataAdmin.php
@@ -8,6 +8,7 @@ use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class UserdataAdmin extends AbstractAdmin
 {
@@ -15,7 +16,16 @@ class UserdataAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('utilisateur', UtilisateurType::class)
+            ->add('utilisateur.id', null, [
+                'label' =>  'Numéro Fonction Publique',
+                'disabled'  =>  true
+            ])
+            ->add('utilisateur.numberid', null, [
+                'label' =>  'Numéro d\'identité'
+            ])
+            ->add('utilisateur.enabled', CheckboxType::class, [
+                'label' =>  'Active'
+            ])
 
 
             // if no type is specified, SonataAdminBundle tries to guess it
@@ -42,15 +52,48 @@ class UserdataAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->addIdentifier('telephone1')
-            ->add('specialite')
-            ->add('genre')
-            ->add('situationmatrimoniale')
-            ->add('regionNaiss')
-            ->add('lieunaiss')
+            ->addIdentifier('utilisateur.id', null, [
+                'label' =>  'Numéro Fonction Publique'
+            ])
+            ->add('utilisateur.numberid', null, [
+                'label' =>  'Numéro d\'identité'
+            ])
+            ->add('utilisateur.username', null, [
+                'label' =>  'Nom d\'utilisateur'
+            ])
+            ->add('utilisateur.firstname', null, [
+                'label' =>  'Prenom'
+            ])
+            ->add('utilisateur.lastname', null, [
+                'label' =>  'Nom'
+            ])
+            ->add('datenaiss', null, [
+                'label' =>  'Date de naissance',
+                'format'    =>  'd-m-Y'
+            ])
+            ->add('specialite', null, [
+                'label' =>  'Spécialité'
+            ])
+            ->add('genre', null, [
+                'label' =>  'Genre'
+            ])
+            ->add('situationmatrimoniale', null, [
+                'label' =>  'Situation matrimoniale'
+            ])
+            ->add('regionNaiss', null, [
+                'label' =>  'Région de naissance'
+            ])
+            ->add('lieunaiss', null, [
+                'label' =>  'Lieu de naissance'
+            ])
             ->add('regionResidence')
-            ->add('lieuresidence')
-            ->add('utilisateur', UtilisateurType::class)
+            ->add('lieuresidence', null, [
+                'label' =>  'Lieu de résidence'
+            ])
+            ->add('telephone1', null, [
+                'label' =>  'Téléphone'
+            ])
+//            ->add('numberid', UtilisateurType::class)
 //            ->add('lieunaiss')
         ;
     }
